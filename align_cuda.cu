4c25b0ee863e96533d9081f47e6c14cbdae49a6f
Mattia35 <mattiaprisco2509@gmail.com>
2025-02-06 13:00:17 +0100

    fixed cuda+mpi version

--- align_cuda.cu
+++ align_cuda.cu
@@ -477,10 +477,11 @@ int main(int argc, char *argv[]) {
 	CUDA_CHECK_KERNEL();
 	MPI_Barrier( MPI_COMM_WORLD );
 	/* 5.2. Copy results back */
+	/*
 	CUDA_CHECK_FUNCTION( cudaMemcpy( pat_found, d_pat_found, sizeof(unsigned long) * pat_number, cudaMemcpyDeviceToHost ) );
 	CUDA_CHECK_FUNCTION( cudaMemcpy( seq_matches, d_seq_matches, sizeof(int) * seq_length, cudaMemcpyDeviceToHost ) );
 	CUDA_CHECK_FUNCTION( cudaMemcpy( &pat_matches, d_pat_matches, sizeof(int), cudaMemcpyDeviceToHost ) );
-
+	*/
 	cudaFree( d_pat_length );
 	cudaFree( d_pattern );
 	cudaFree( d_seq_matches );
