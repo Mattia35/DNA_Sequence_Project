9f3a6695bc2ce3ad007eb362b994dae3effc8620
Mattia35 <mattiaprisco2509@gmail.com>
2025-02-06 16:22:10 +0100

    fixed cuda+mpi version

--- align_cuda.cu
+++ align_cuda.cu
@@ -276,10 +276,8 @@ int main(int argc, char *argv[]) {
 	CUDA_CHECK_FUNCTION(cudaGetDeviceCount(&device_count));
     CUDA_CHECK_FUNCTION(cudaSetDevice(rank%device_count)); 
 	//printa quanti device ci sono e quale è stato scelto
-	if (rank==0){
-		printf("Device count: %d\n",device_count);
-		printf("Device scelto: %d\n",rank%device_count);
-	}
+	printf("Device count: %d\n",device_count);
+	printf("Device scelto: %d\n",rank%device_count);
 
 	/* 2. Initialize data structures */
 	/* 2.1. Skip allocate and fill sequence */
