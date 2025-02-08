b3c962a4da7d39d0ba9ad446028145c5caeaa2f6
Mattia35 <mattiaprisco2509@gmail.com>
2025-02-08 13:02:43 +0100

    fixed cuda+mpi version

--- align_cuda.cu
+++ align_cuda.cu
@@ -464,6 +464,7 @@ int main(int argc, char *argv[]) {
 	size_t sharedMemSize = seq_length * sizeof(char);
 	
 	pattern_search_kernel<<<numBlocks, blockSize, sharedMemSize>>>(d_sequence, d_pat_matches, d_pat_found, d_seq_matches, d_pat_length, d_pattern, seq_length, pat_number, inizio, fine);
+	printf("Rank: %d, inizio: %d, fine: %d\n", rank, inizio, fine);
 	CUDA_CHECK_FUNCTION( cudaDeviceSynchronize() );
 	MPI_Barrier( MPI_COMM_WORLD );
 	/* 5.2. Copy results back */
