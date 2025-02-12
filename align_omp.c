4d7227793fd74853ab8b8f540f163b046c687a98
Mattia35 <mattiaprisco2509@gmail.com>
2025-02-12 23:42:44 +0100

    fixed cuda+mpi version

--- align_omp.c
+++ align_omp.c
@@ -367,7 +367,7 @@ int main(int argc, char *argv[]) {
 	}
 	/* 5. Search for each pattern */ 
 	omp_set_num_threads(4);
-	#pragma omp parallel for private(lind) reduction(+:pat_matches) reduction(+:seq_matches[:seq_length]) 
+	#pragma omp parallel for private(lind) reduction(+:pat_matches) reduction(+:seq_matches[:seq_length]) schedule(dynamic)
 	for( int pat=0; pat < pat_number; pat++ ) {
 		/* 5.1. For each posible starting position */
 		for( unsigned long start=0; start <= seq_length - pat_length[pat]; start++) {
