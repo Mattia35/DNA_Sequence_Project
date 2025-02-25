a0a0b49ddde709f26209054f69ef71a2f1c32ea4
Mattia35 <mattiaprisco2509@gmail.com>
2025-02-25 11:42:30 +0100

    standard version

--- script.sh
+++ script.sh
@@ -3,13 +3,21 @@
 N_RUNS=150  # Numero di esecuzioni
 OUTPUT_FILE="tempi.txt"
 
-echo "Eseguo $N_RUNS test e salvo i risultati in $OUTPUT_FILE"
+echo "Eseguo $N_RUNS test e salvo la media in $OUTPUT_FILE"
 > $OUTPUT_FILE  # Svuota il file prima di scrivere nuovi risultati
+
+total_time=0  # Variabile per la somma dei tempi
 
 for i in $(seq 1 $N_RUNS); do
     # Esegui il programma e prendi solo la riga con "Time:"
     tempo=$(./align_omp 16777216 0.1 0.3 0.35 64 8 2 64 32 2 8388608 4194304 M 609823 | grep "Time:" | awk '{print $2}')
-    echo $tempo >> $OUTPUT_FILE  # Salva solo il tempo in tempi.txt
+    total_time=$(echo "$total_time + $tempo" | bc)  # Somma i tempi
+  
 done
 
-echo "Test completati! Tempi salvati in $OUTPUT_FILE"
+# Calcola la media
+tempo_medio=$(echo "scale=6; $total_time / $N_RUNS" | bc)
+
+echo "$tempo_medio" > $OUTPUT_FILE  # Salva solo la media nel file
+
+echo "Test completati! Media salvata in $OUTPUT_FILE"
